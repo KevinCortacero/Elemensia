a937f2b1649e31c1db4a95dfd98e8f7e2c4631da
Twarz <cortacero.k31130@gmail.com>
2017-03-01 21:19:17 +0100

    Game ladders

--- Elemens-core/src/com/elemens/Hero.java
+++ Elemens-core/src/com/elemens/Hero.java
@@ -6,23 +6,26 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 
-public class Hero extends DynamicGameObject{
-	
-	private Rectangle bottom, top, left, right, center;
-	
+public class Hero extends DynamicGameObject {
+
+	private Rectangle bottom, top, left, right;
+	Rectangle center;
+	boolean canClimb;
+
 	public Hero(int x, int y, int width, int height) {
 		super(x, y, width, height);
-		this.bottom = new Rectangle(x+10, y, width-20, height/4);
-		this.top    = new Rectangle(x+10, y + height*3/4, width- 20, height/4);
-		this.left   = new Rectangle(x, y + 10, width/4, height - 20);
-		this.right  = new Rectangle(x + width*3/4, y + 10, width/4, height - 20);
-		this.center = new Rectangle(x + width/4, y + height/4, width/2, height/2);
+		this.bottom = new Rectangle(x + 10, y, width - 20, 10);
+		this.top = new Rectangle(x + 10, y + height - 10, width - 20, 10);
+		this.left = new Rectangle(x, y + 20, 10, height - 40);
+		this.right = new Rectangle(x + width - 10, y + 20, 10, height - 40);
+		this.center = new Rectangle(x + 10, y + 20, width - 20, height - 30);
+		this.canClimb = false;
 	}
-	
-	public Hitbox isColliding(Rectangle r){
+
+	public Hitbox isColliding(Rectangle r) {
 		if (this.center.overlaps(r))
 			return Hitbox.CENTER;
-		if (this.bottom.overlaps(r)){
+		if (this.bottom.overlaps(r)) {
 			this.resetJump();
 			return Hitbox.BOTTOM;
 		}
@@ -32,53 +35,56 @@ public class Hero extends DynamicGameObject{
 			return Hitbox.RIGHT;
 		if (this.top.overlaps(r))
 			return Hitbox.TOP;
+
 		return Hitbox.NONE;
 	}
-	
+
 	@Override
 	public void setPosition(float x, float y) {
 		super.setPosition(x, y);
-		this.bottom.setPosition(x+10, y);
-		this.top.setPosition(x+10, y + this.body.height*3/4);
-		this.left.setPosition(x, y + 10);
-		this.right.setPosition(x + this.body.width*3/4, y + 10);
-		this.center.setPosition(x + this.body.width/4, y + this.body.height/4);
+		this.bottom.setPosition(x + 10, y);
+		this.top.setPosition(x + 10, y + this.body.height - 10);
+		this.left.setPosition(x, y + 20);
+		this.right.setPosition(x + this.body.width - 10, y + 20);
+		this.center.setPosition(x + 10, y + 20);
 	}
 
 	public void draw(ShapeRenderer sr) {
-		//this.drawHitBox(sr, this.body, Color.GOLD);
+		// this.drawHitBox(sr, this.body, Color.GOLD);
 		this.drawHitBox(sr, this.left, Color.GREEN);
 		this.drawHitBox(sr, this.right, Color.GREEN);
 		this.drawHitBox(sr, this.bottom, Color.BLUE);
 		this.drawHitBox(sr, this.top, Color.BLUE);
 		this.drawHitBox(sr, this.center, Color.RED);
 	}
-	
-	private void drawHitBox(ShapeRenderer sr, Rectangle r, Color c){
+
+	private void drawHitBox(ShapeRenderer sr, Rectangle r, Color c) {
 		sr.rect(r.x, r.y, r.width, r.height, c, c, c, c);
 	}
 
 	public void updateInput() {
-		if(Gdx.input.isKeyPressed(Input.Keys.Z)){
-			this.climb();
+		if (Gdx.input.isKeyPressed(Input.Keys.Z)) {
+			if (this.canClimb) {
+				this.climb();
+			}
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.D)){
+		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
 			this.moveRight(Gdx.graphics.getDeltaTime());
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.Q)){
+		if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
 			this.moveLeft(Gdx.graphics.getDeltaTime());
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+		if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
 			this.jump();
 		}
 	}
 
 	public float getCenterX() {
-		return this.body.x + this.body.width/2;
+		return this.body.x + this.body.width / 2;
 	}
-	
+
 	public float getCenterY() {
-		return this.body.y + this.body.height/2;
+		return this.body.y + this.body.height / 2;
 	}
-	
+
 }
--- Elemens-core/src/com/elemens/World.java
+++ Elemens-core/src/com/elemens/World.java
@@ -22,17 +22,26 @@ public class World implements Disposable{
 		this.solids.add(new Solid(0, 0, 2000, 140));
 		this.solids.add(new Solid(0, 140, 20, 1000));
 		this.solids.add(new Solid(50, 200, 200, 50));
-		this.solids.add(new Solid(360, 380, 94, 20));
+		
+		// Ladder
+		this.solids.add(new Solid(372, 396, 70, 4));
+		this.solids.add(new Solid(302, 140, 70, 260));
+		this.solids.add(new Solid(442, 380, 70, 20));
 		this.ladders = new ArrayList<Solid>();
 		this.ladders.add(new Solid(372, 140, 70, 260));
-		this.hero = new Hero(200, 100, 74, 107);
+		
+		// Steps
+		/*for (int i = 0; i < 1000; i++){
+			this.solids.add(new Solid(800+i*2, 150+i, 2, 1));
+		}*/
+		this.hero = new Hero(600, 200, 74, 107);
 	}
-	
+
 	public void draw(SpriteBatch sb){
 		sb.draw(this.background, 0, 0);
 		this.hero.draw(sb);
 	}
-	
+
 	public void draw(ShapeRenderer sr){
 		this.hero.draw(sr);
 		for (Solid s : this.solids)
@@ -43,40 +52,44 @@ public class World implements Disposable{
 
 	public void update(){
 		this.hero.update(Gdx.graphics.getDeltaTime());
-		boolean isClimbing = false;
+		this.hero.canClimb = false;
 		for (Solid l : this.ladders){
-			if (this.hero.isColliding(l.body) == Hitbox.CENTER){
-				isClimbing = true;
+			if (this.hero.center.overlaps(l.body)){
+				this.hero.canClimb = true;
 				break;
 			}
 		}
-		if (!isClimbing){
-			for (Solid s : this.solids){
-				switch(this.hero.isColliding(s.body)){
-				case NONE:
-					break;
-				case CENTER:
-					break;
-				case BOTTOM:
-					this.hero.stopV(s.body.y + s.body.height);
-					break;
-				case LEFT:
+		for (Solid s : this.solids){
+			switch(this.hero.isColliding(s.body)){
+			case NONE:
+				break;
+			case CENTER:
+				break;
+			case BOTTOM:
+				this.hero.stopV(s.body.y + s.body.height);
+				break;
+			case LEFT:
+				if (!this.hero.canClimb)
 					this.hero.stopH(s.body.x + s.body.width +1);
-					break;
-				case RIGHT:
+				break;
+			case RIGHT:
+				if (!this.hero.canClimb)
 					this.hero.stopH(s.body.x - this.hero.body.width -1);
-					break;
-				case TOP:
+				break;
+			case TOP:
+				if (!this.hero.canClimb)
 					this.hero.stopV(s.body.y - this.hero.body.height);
-					break;
-				
-				}
+				break;
 			}
 		}
-		
+
 		this.hero.updateInput();
+		
+		if (this.hero.body.y < -100){
+			this.hero.setPosition(600, 200);
+		}
 	}
-	
+
 	@Override
 	public void dispose() {
 		this.background.dispose();
@@ -96,7 +109,7 @@ public class World implements Disposable{
 		else{
 			camX = heroX;
 		}
-		
+
 		if (heroY <= 450){
 			camY = 450;
 		}
