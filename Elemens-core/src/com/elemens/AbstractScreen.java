957a4b741ad8a773a9ebb846db43f724240b0c0e
twarz <twarz@world>
2017-07-25 13:45:38 +0200

    nothing

--- Elemens-core/src/com/elemens/AbstractScreen.java
+++ Elemens-core/src/com/elemens/AbstractScreen.java
@@ -19,9 +19,8 @@ public abstract class AbstractScreen extends Stage implements Screen {
 	@Override
 	public void render(float delta) {
 		// Clear screen
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT 
-				     | GL20.GL_DEPTH_BUFFER_BIT 
-				     | (Gdx.graphics.getBufferFormat().coverageSampling ? GL20.GL_COVERAGE_BUFFER_BIT_NV : 0));
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT
+				| (Gdx.graphics.getBufferFormat().coverageSampling ? GL20.GL_COVERAGE_BUFFER_BIT_NV : 0));
 
 		// Calling to Stage methods
 		super.act(delta);
