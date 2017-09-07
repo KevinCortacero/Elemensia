4acb506af79c840368f7ceb9893ed520e224ff91
Pierre <pierre31m@gmail.com>
2017-09-07 16:56:43 +0200

    ras

--- Elemens-desktop/src/com/elemens/desktop/DesktopLauncher.java
+++ Elemens-desktop/src/com/elemens/desktop/DesktopLauncher.java
@@ -4,10 +4,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.elemens.ElemensiaConfiguration;
 import com.elemens.ElemensiaGame;
+import com.elemens.SplashScreen;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new ElemensiaConfiguration(800, 450, true);
+		LwjglApplicationConfiguration config = new ElemensiaConfiguration(SplashScreen.WINDOW_WIDTH, SplashScreen.WINDOW_HEIGHT, true);
 		
 		ElemensiaGame elemensia = new ElemensiaGame();
 		new LwjglApplication(elemensia, config);
